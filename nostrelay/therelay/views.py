ac350fa1a3168bf0041db6b518615a75e242b2e7
Paula Livingstone <paula@paulalivingstone.com>
2023-05-11 11:43:08 +0000

    Added my_view

--- nostrelay/therelay/views.py
+++ nostrelay/therelay/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from channels.layers import get_channel_layer
+from asgiref.sync import async_to_sync
+from . import consumers
+
+def my_view(request):
+    channel_layer = get_channel_layer()
+    async_to_sync(channel_layer.group_add)("my_group", request.channel_name)
+    consumer = consumers.MyConsumer()
+    await consumer.connect()
+    message = await consumer.receive()
+    await consumer.send({"type": "hello"})
+    await consumer.disconnect()
+    return render(request, "my_template.html")

3e95663b82c36a9dca7081b79dc4d4d41e445399
Paula Livingstone <paula@paulalivingstone.com>
2023-05-12 08:36:31 +0000

    ASGI running with rationalised connect func

--- nostrelay/therelay/consumers.py
+++ nostrelay/therelay/consumers.py
@@ -5,19 +5,19 @@ import json, asyncio, websockets
 class MyConsumer(AsyncWebsocketConsumer):
 
     async def connect(self):
-        print("hiho")
-        self.websocket = await websockets.connect('ws://localhost:8765')
+        print("hiho1")
+        #self.websocket = await websockets.connect('ws://165.22.113.110:13167/ws/my-websocket/')
 
     async def receive(self):
-        print("hiho")
+        print("hiho2")
         message = await self.websocket.recv()
         return json.loads(message)
 
     async def send(self, data):
-        print("hiho")
+        print("hiho3")
         message = json.dumps(data)
         await self.websocket.send(message)
 
     async def disconnect(self):
-        print("hiho")
+        print("hiho4")
         await self.websocket.close()
